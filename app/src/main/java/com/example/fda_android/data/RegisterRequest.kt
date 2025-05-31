5a4740d1c5f39907d253d54a9224aab782607229
anishkumarbiswal <anish.biswal@mobikwik.com>
2025-05-31 23:40:43 +0530

    added parcelize data

--- app/src/main/java/com/example/fda_android/data/RegisterRequest.kt
+++ app/src/main/java/com/example/fda_android/data/RegisterRequest.kt
@@ -1,7 +1,11 @@
 package com.example.fda_android.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class RegisterRequest(
     val phoneNumber: String,
     val password: String
-)
+) : Parcelable
 
