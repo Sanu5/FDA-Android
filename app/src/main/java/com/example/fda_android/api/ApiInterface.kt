8772df029a652436baa4668edb6b0c900716d8ce
Himanshu Khurana <himanshu16khurana@gmail.com>
2025-06-01 00:07:09 +0530

    few provisional changes in interface.

--- app/src/main/java/com/example/fda_android/api/ApiInterface.kt
+++ app/src/main/java/com/example/fda_android/api/ApiInterface.kt
@@ -1,11 +1,15 @@
 package com.example.fda_android.api
 
+import com.example.fda_android.data.CartResponse
+import com.example.fda_android.data.HomeResponse
 import com.example.fda_android.data.RegisterRequest
 import com.example.fda_android.data.RegisterResponse
+import com.example.fda_android.data.RestaurantItem
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ApiInterface {
     @POST("/auth/register")
@@ -14,7 +18,23 @@ interface ApiInterface {
     @POST("/auth/login")
     suspend fun loginUser(@Body request: RegisterRequest): RegisterResponse
 
+    @GET("/home")
+    suspend fun getHomeData(): Response<HomeResponse>
 
+    @GET("/restaurants")
+    suspend fun getRestaurants(): Response<List<RestaurantItem>>
+
+    @POST("/cart")
+    suspend fun addCartItem(): Response<Boolean>
+
+    @GET("/cart")
+    suspend fun viewCart(): Response<CartResponse>
+
+    @PUT("/cart")
+    suspend fun updateCart(): Response<Boolean>
+
+    @POST("/orders")
+    suspend fun placeOrder(): Response<Boolean>
 
 
 }
