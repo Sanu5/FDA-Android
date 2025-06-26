9f5aa0439146533361c46d5283778345b1cd5244
Himanshu Khurana <himanshu16khurana@gmail.com>
2025-06-26 12:17:31 +0530

    added CartItemAdapter.kt and CartItemViewHolder.kt

--- app/src/main/java/com/example/fda_android/ui/adapter/CartAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.fda_android.ui.adapter
-
-class CartAdapter {
-}
---
+++ app/src/main/java/com/example/fda_android/ui/adapter/CartItemAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.fda_android.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fda_android.data.ItemData
+import com.example.fda_android.viewholder.CartItemViewHolder
+
+class CartItemAdapter(
+    private val items: List<ItemData>,
+    private val onQuantityChanged: (itemId: String, newQuantity: Int) -> Unit
+) : RecyclerView.Adapter<CartItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartItemViewHolder {
+        return CartItemViewHolder.getViewHolder(parent, onQuantityChanged)
+    }
+
+    override fun onBindViewHolder(holder: CartItemViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+}
