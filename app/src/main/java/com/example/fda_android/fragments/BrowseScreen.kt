bf11766a79ab486640077cd8e1e7c900e9943af9
Anish Biswal <biswalworkanish@gmail.com>
2025-06-27 12:43:37 +0530

    code cleanup

--- app/src/main/java/com/example/fda_android/fragments/BrowseScreen.kt
+++ app/src/main/java/com/example/fda_android/fragments/BrowseScreen.kt
@@ -33,6 +33,8 @@ class BrowseScreen: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.fetchRestaurantList()
+
         lifecycleScope.launchWhenStarted {
             viewModel.resState.collect { state ->
                 when (state) {
